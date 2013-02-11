3c16da60c0545395e3b3d021334b04cb5dcd82d7
Hernán Meydac Jean <hernan.meydac@southworks.net>
2013-02-11 12:56:07 -0300

    - Deshabilité el borrado de la base de datos si el modelo cambió.

--- Code/Bishop/Bishop.UI.Web/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
+++ Code/Bishop/Bishop.UI.Web/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
@@ -3,14 +3,17 @@ using Devtalk.EF.CodeFirst;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Bishop.UI.Web.App_Start.DontDropDbJustCreateTablesIfModelChangedStart), "Start")]
 
-namespace Bishop.UI.Web.App_Start {
+namespace Bishop.UI.Web.App_Start
+{
     using Bishop.Model;
 
-    public static class DontDropDbJustCreateTablesIfModelChangedStart {
-        public static void Start() {
+    public static class DontDropDbJustCreateTablesIfModelChangedStart
+    {
+        public static void Start()
+        {
             // Uncomment this line and replace CONTEXT_NAME with the name of your DbContext if you are 
             // using your DbContext to create and manage your database
-           // Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<FormsContext>());
+            Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<FormsContext>());
         }
     }
 }
