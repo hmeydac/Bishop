0dfe71bb76e010ad181580296d9182735ad1193e
Hernán Meydac Jean <hernan.meydac@southworks.net>
2013-02-16 10:27:46 -0300

    - Se arreglaron tests de CompanyService.

--- Code/Bishop/Bishop.Services.Tests/CompanyServiceTest.cs
+++ Code/Bishop/Bishop.Services.Tests/CompanyServiceTest.cs
@@ -20,7 +20,9 @@
         {
             // Arrange
             var expected = this.GetSampleData();
-            var unitOfWork = this.SetupUnitOfWork(expected);
+            var unitOfWork = new Mock<IUnitOfWork>();
+            string[] includeNames = { "Contacts" };
+            unitOfWork.Setup(u => u.Query<Company>(includeNames)).Returns(expected.AsQueryable());
             var service = new CompanyService(unitOfWork.Object);
 
             // Act
@@ -36,7 +38,7 @@
             // Arrange
             var unitOfWorkMock = new Mock<IUnitOfWork>(MockBehavior.Strict);
             var expectedData = this.GetSampleData().OrderBy(f => f.Name).ToArray();
-            unitOfWorkMock.Setup(u => u.Query<Company>()).Returns(expectedData.AsQueryable());
+            unitOfWorkMock.Setup(u => u.Query<Company>(It.IsAny<string>())).Returns(expectedData.AsQueryable());
             var service = new CompanyService(unitOfWorkMock.Object);
 
             // Act
@@ -50,7 +52,8 @@
             // Arrange
             var sampleData = this.GetSampleData();
             Company expected = sampleData[1]; // Arbritary Value
-            var unitOfWork = this.SetupUnitOfWork(sampleData);
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(u => u.Query<Company>()).Returns(sampleData.AsQueryable());
             var service = new CompanyService(unitOfWork.Object);
 
             // Act  
@@ -59,14 +62,6 @@
             // Assert
             Assert.IsNotNull(actual);
             Assert.AreSame(expected, actual);
-        }
-
-        private Mock<IUnitOfWork> SetupUnitOfWork(IEnumerable<Company> data)
-        {
-            var unitOfWork = new Mock<IUnitOfWork>();
-            string[] includeNames = { "Contacts" };
-            unitOfWork.Setup(u => u.Query<Company>(includeNames)).Returns(data.AsQueryable());
-            return unitOfWork;
         }
 
         private Company[] GetSampleData()
